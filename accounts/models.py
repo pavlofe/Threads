d7cee92f1e829771d11f55805058d8586e6c25da
pavlofe <fedakpavlo70@gmail.com>
2025-11-20 18:19:08 +0200

    Редагував models.py, та зробив реєстрацію у views.py

--- accounts/models.py
+++ accounts/models.py
@@ -37,10 +37,28 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=(
+            ('male', 'Male'),
+            ('female', 'Female'),
+        ),
+        null=True,
+        blank=True
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
+
 
 
 class Notification(models.Model):
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.hashers import make_password
+from .models import Profile
+
 
 from accounts.forms import LoginForm, RegisterForm
 
@@ -22,3 +26,30 @@ class RegisterView(CreateView):
     template_name = 'accounts/register.html'
     form_class = RegisterForm
     success_url = reverse_lazy('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        gender = request.POST.get('gender')
+        phone = request.POST.get('phone')
+
+        user = User.objects.create(
+            username=username,
+            email=email,
+            password=make_password(password)
+        )
+
+        Profile.objects.create(
+            user=user,
+            gender=gender,
+            phone=phone
+        )
+
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'accounts/register.html')
