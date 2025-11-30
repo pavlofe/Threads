5e5596965db71f7920ed6c49bff248780c6c53db
pavlofe <fedakpavlo70@gmail.com>
2025-11-30 16:17:08 +0200

    Зробив views.py для перегляду та релагування акаунта

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import profile_view, edit_profile
+
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('profile/', profile_view, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
@@ -6,8 +7,6 @@ from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.hashers import make_password
 from .models import Profile
-
-
 from accounts.forms import LoginForm, RegisterForm
 
 
@@ -47,10 +46,38 @@ def register(request):
         Profile.objects.create(
             user=user,
             gender=gender,
-            phone=phone
+            phone=phone,
+            age=age
         )
 
         login(request, user)
         return redirect('home')
 
     return render(request, 'accounts/register.html')
+
+
+@login_required
+def profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
+
+
+@login_required
+def edit_profile(request):
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        profile.gender = request.POST.get('gender')
+        profile.phone = request.POST.get('phone')
+        profile.age = request.POST.get('age')
+        profile.save()
+
+        request.user.email = request.POST.get('email')
+        request.user.save()
+
+        return redirect('profile')
+
+    return render(request, 'accounts/edit_profile.html', {
+        'profile': profile,
+        'user': request.user
+    })
